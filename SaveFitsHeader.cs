7e6dd034434e6e0c047505cac7525eee8c60640e
Sebastian Godelet <sebastian.godelet@outlook.com>
2022-04-04 14:51:53 +1000

    Add Safe-FitsHeader

---
+++ SaveFitsHeader.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Management.Automation;
+
+namespace PSFits
+{
+    [Cmdlet(VerbsData.Save, "FitsHeader")]
+    [OutputType(typeof(FitsFileHandle))]
+    public class SaveFitsHeader : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = true,
+            Position = 0,
+            ValueFromPipeline = false,
+            ValueFromPipelineByPropertyName = true)]
+        public FitsFileHandle FitsFile { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            if (FitsFile != null && FitsFile.PrimaryHDU.Rewriteable)
+            {
+                FitsFile.PrimaryHDU.Rewrite();
+                WriteObject(FitsFile);
+            }
+            else
+            {
+                throw new InvalidDataException($"Fits file {FitsFile?.FullName} is not rewritable");
+            }
+        }
+    }
+}
